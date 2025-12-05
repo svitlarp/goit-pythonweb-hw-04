02a2317e754cf91d83cf2daa369f55c5b069ddd4
svitlarp <svitlanka.ripa@gmail.com>
2025-12-05 14:01:39 +0100

    rewrite to async sorting, add asyncio lib, async/await syntax, get event loop

--- sorter.py
+++ sorter.py
@@ -1,5 +1,6 @@
 import argparse
 import shutil
+import asyncio
 from pathlib import Path
 
 
@@ -14,38 +15,40 @@ def parse_args():
         return args.filename
     
 
-def apply_file_type_method(path):
+async def apply_file_type_method(path):
     if path.is_dir():
-        handleDir(path)
+        await handleDir(path)
     else:
-        handleFile(path)
+        await handleFile(path)
         
 
-def handleDir(dir: Path):
+async def handleDir(dir: Path):
+    tasks = []
     for file in dir.iterdir():
         if file.is_file():
-            handleFile(file)
+            tasks.append(handleFile(file))
+    await asyncio.gather(*tasks)
 
                  
-def handleFile(file: Path):
+async def handleFile(file: Path):
     ext = file.suffix.lstrip(".")
     source_dir = file.parent
     target_dir = source_dir / ext
-    if target_dir.exists():
-        # Move file to the directory
-        shutil.move(file, target_dir)
-    else:
-        # Create the directory
-        target_dir.mkdir()
 
-        # Move file to the directory
-        shutil.move(file, target_dir)
+    loop = asyncio.get_event_loop()
 
-def main():
+    if not target_dir.exists():
+        # Move file to the directory
+        await loop.run_in_executor(None, target_dir.mkdir)
+
+    # Move file to the directory
+    await loop.run_in_executor(None, shutil.move, file, target_dir)
+
+async def main():
     filename = parse_args()
-    apply_file_type_method(Path(filename))
+    await apply_file_type_method(Path(filename))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
 
