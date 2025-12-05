ad671849e2de4290ed3dd6abdbf18e215f6d33c2
svitlarp <svitlanka.ripa@gmail.com>
2025-12-05 16:04:28 +0100

    improve to use asyncio lib, add readme description

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # goit-pythonweb-hw-04
 async python file-sorter
 
+A program that reads source and destination directories from the command line and copies all files from the source directory to the destination directory based on their file extensions.
+ 
 
+```python3 sorter.py  -s random-files -o sorteddir```
+
+where:
+ - **-s**  source directory
+ - **-o** output directory
 
--- sorter.py
+++ sorter.py
@@ -1,9 +1,10 @@
 import argparse
 import shutil
 import logging
+import asyncio
 from pathlib import Path
 
-
+# Logging program
 logging.basicConfig(
     format='%(asctime)s, %(message)s',
     level=logging.WARNING,
@@ -14,32 +15,37 @@ logging.basicConfig(
 )
 
 def parse_args():
-    """Function that parse arguments from cli"""
+    """Parses command-line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source', required=True, type=str, help='Source folder')
     parser.add_argument('-o', '--output', required=True, type=str, help='Output folder')
     return parser.parse_args()    
 
-def read_folder(source:Path, output:Path):
+async def read_folder(source:Path, output:Path):
+    """Recursively reads all files in a directory and its subdirectories."""
+    tasks = []
     for f in source.iterdir():
         if f.is_dir():
-            read_folder(f, output)
+            tasks.append(read_folder(f, output))
         else:
-            copy_file(f, output)
+            tasks.append(copy_file(f, output))
+    await asyncio.gather(*tasks)
 
-def copy_file(file:Path, output:Path):
+async def copy_file(file:Path, output:Path):
+    """Copies a file into a subforlder based on its extension"""
     try:
         ext = file.suffix.lstrip(".").lower()
         dest_dir = output / ext
         if not dest_dir.exists():
             dest_dir.mkdir(parents=True)
 
-        shutil.copy(file, dest_dir)
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, shutil.copy2, file, dest_dir)
     except Exception as e:
         logging.error(f"Error copying {file}: {e}")
 
                  
-def main():
+async def main():
     args = parse_args()
     source = Path(args.source).resolve()
     output = Path(args.output).resolve()
@@ -48,9 +54,9 @@ def main():
         print("source dir does not exist")
         return
     
-    read_folder(source, output)
+    await read_folder(source, output)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
 
